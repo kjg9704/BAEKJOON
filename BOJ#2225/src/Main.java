5341ab7c89febc4ffbbc5b2621dc4796a9680830
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-02-19 22:10:55 +0900

    합분해
    Solved(풀이참고함)

--- BOJ#2225/src/Main.java
+++ BOJ#2225/src/Main.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int [][] dp = new int[K + 1][N + 1];
+		dp[1][N] = 1;
+		
+		for (int i = 0; i <= N ; i++) {
+            dp[1][i] = 1;
+        }
+        // N != 0
+        for (int i = 1; i <= K ; i++) {
+            for (int j = 0; j <= N ; j++) {
+                for (int k = j; k >=0 ; k--) {
+                    dp[i][j] += dp[i - 1][j - k];
+                    dp[i][j] %= 1000000000;
+                }
+            }
+        }
+		System.out.println(dp[K][N]);
+		
+		sc.close();
 	}
 
 }

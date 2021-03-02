3ea07d26dcd42d1bb461397b7760f3673945dec2
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-03-03 04:46:55 +0900

    수 정렬하기 5
    Solved

---
+++ BOJ#15688/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int [] arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+		
+		for(int i : arr) {
+			sb.append(i + "\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
+}

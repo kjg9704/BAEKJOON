4f464fa5b799df14ea488e96d908fec0a87cbc93
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-03-19 01:02:40 +0900

    특정한 최단 경로
    Solved

--- BOJ#1504/src/Main.java
+++ BOJ#1504/src/Main.java
@@ -31,26 +31,54 @@ public class Main {
 			temp = br.readLine().split(" ");
 			int start = Integer.parseInt(temp[0]);
 			int end = Integer.parseInt(temp[1]);
-			int dis = Integer.parseInt(temp[1]);
+			int dis = Integer.parseInt(temp[2]);
 			list[start].add(new Edge(end, dis));
+			list[end].add(new Edge(start, dis));
 		}
 		temp = br.readLine().split(" ");
 		int node1 = Integer.parseInt(temp[0]);
 		int node2 = Integer.parseInt(temp[1]);
 		br.close();
 		PriorityQueue<Edge> que = new PriorityQueue<>();
-		boolean[] visit = new boolean[N + 1];
-		Edge[] distance = new Edge[N + 1];
+		boolean[] visit;
+		Edge[][] distance = new Edge[N + 1][N + 1];
 		for(int i = 1; i <= N; i++) {
-			if(i == 1) {
-				distance[i] = new Edge(i, 0);
-			}else {
-				distance[i] = new Edge(i, Integer.MAX_VALUE);
+			for(int j = 1; j <= N; j++) {
+				if(i == j) {
+					distance[i][j] = new Edge(j, 0);
+				}else {
+				distance[i][j] = new Edge(j, Integer.MAX_VALUE);
+				}
 			}
-			que.add(distance[i]);
 		}
-		
-
+		for(int i = 1; i <= N; i++) {
+			if(!(i == 1 || i == node1 || i == node2)) {
+				continue;
+			}
+			visit = new boolean[N + 1];
+			for(int j = 1; j <= N; j++) {
+				que.add(distance[i][j]);
+			}
+			while(!que.isEmpty()) {
+				Edge edge = que.poll();
+				if(edge.distance == Integer.MAX_VALUE) break;
+				for (Edge next : list[edge.node]) {
+					if(visit[next.node]) continue;
+					if(distance[i][next.node].distance > distance[i][edge.node].distance + next.distance) {
+						distance[i][next.node].distance = distance[i][edge.node].distance + next.distance;
+						distance[next.node][i].distance = distance[i][edge.node].distance + next.distance;
+						que.remove(distance[i][next.node]);
+						que.add(distance[i][next.node]);
+					}
+				} 
+				visit[edge.node] = true;
+			}
+		}
+		if(distance[1][node1].distance == Integer.MAX_VALUE || distance[1][node2].distance == Integer.MAX_VALUE || distance[1][N].distance == Integer.MAX_VALUE || distance[node1][node2].distance == Integer.MAX_VALUE) {
+			System.out.println(-1);
+		}else {
+			long result = Math.min((long)distance[1][node1].distance + (long)distance[node1][node2].distance + (long)distance[node2][N].distance, (long)distance[1][node2].distance + (long)distance[node2][node1].distance + (long)distance[node1][N].distance);
+			System.out.println(result);
+		}
 	}
-
 }

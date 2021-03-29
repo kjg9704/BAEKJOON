d6b70b2d445d5a5f936abb9a3107d087eb9350ce
김종건 <48309937+kjg9704@users.noreply.github.com>
2021-03-29 23:43:30 +0900

    문자열 폭발
    Solved

--- BOJ#9935/src/Main.java
+++ BOJ#9935/src/Main.java
@@ -7,24 +7,37 @@ public class Main {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
+		char[] str = br.readLine().toCharArray();
 		String bomb = br.readLine();
 		br.close();
-		Stack<String> stack = new Stack<>();
-		stack.add(str);
-		while(!stack.isEmpty()) {
-			String next = stack.peek().replace(bomb, "");
-			if(next.equals(stack.pop())) {
-				if(next.length() == 0) {
-					System.out.println("FRULA");
-				}else {
-					System.out.println(next);
+		Stack<Character> stack = new Stack<>();
+		int bombLen = bomb.length();
+		for(int i = 0; i < str.length; i++) {
+			stack.add(str[i]);
+			if(stack.size() >= bombLen) {
+				boolean check = true;
+				for(int j = 0; j < bombLen; j++) {
+					if(!stack.get(stack.size() - bombLen + j).equals(bomb.charAt(j))) {
+						check = false;
+						break;
+					}
 				}
-			}else {
-				stack.push(next);
+				if(check) {
+					for(int j = 0; j < bombLen; j++) {
+						stack.pop();
+					}
+				}
 			}
 		}
-		
+		if(stack.size() == 0) {
+			System.out.println("FRULA");
+		}else {
+			StringBuilder sb = new StringBuilder();
+			for(char c : stack) {
+				sb.append(c);
+			}
+			System.out.println(sb);
+		}
 	}
 
 }

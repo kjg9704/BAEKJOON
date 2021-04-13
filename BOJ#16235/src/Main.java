e087fe689a29c168d5b3b7cde290bb19528d6b72
김종건 <kjg9704@daum.net>
2021-04-14 03:20:05 +0900

    나무 재테크(진행중)

--- BOJ#16235/src/Main.java
+++ BOJ#16235/src/Main.java
@@ -1,10 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Stack;
 
 class Ground{
 	int nurtient;
-	PriorityQue
+	int increase;
+	Deque<Integer> tree = new ArrayDeque<>();
+	ArrayList<Integer> dead = new ArrayList<>();
+
+	public Ground(int increase) {
+		this.nurtient = 5;
+		this.increase = increase;
+	}
+
 }
 public class Main {
 
@@ -19,11 +31,12 @@ public class Main {
 		N = Integer.parseInt(temp[0]);
 		M = Integer.parseInt(temp[1]);
 		K = Integer.parseInt(temp[2]);
-		int[][] matrix = new int[N + 1][N + 1];
+		Ground[][] matrix = new Ground[N + 1][N + 1];
 		for(int i = 1; i <= N; i++) {
 			temp = br.readLine().split(" ");
 			for(int j = 0; j < N; j++) {
-				matrix[i][j + 1] = Integer.parseInt(temp[j]);
+				int increase = Integer.parseInt(temp[j]);
+				matrix[i][j + 1] = new Ground(increase);
 			}
 		}
 		for(int i = 0; i < M; i++) {
@@ -31,15 +44,84 @@ public class Main {
 			int X = Integer.parseInt(temp[0]);
 			int Y = Integer.parseInt(temp[1]);
 			int age = Integer.parseInt(temp[2]);
+			matrix[X][Y].tree.add(age);
 		}
-		
+
 		for(int i = 0; i < K; i++) {
-			cycle();
+			cycle(matrix);
 		}
+		int result = 0;
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				result += matrix[i][j].tree.size();
+			}
+		}
+		System.out.println(result);
 	}
-	
-	static void cycle() {
-		for(int )
+
+	static void cycle(Ground[][] matrix) {
+		for(int i = 1; i <= N; i++) { //봄
+			for(int j = 1; j <= N; j++) {
+				if(!matrix[i][j].tree.isEmpty()) {
+					Stack<Integer> grow = new Stack<>();
+					int index = 0;
+					int size = matrix[i][j].tree.size();
+					for(int z = 0; z < size; z++) {
+						if(matrix[i][j].nurtient >= matrix[i][j].tree.peekFirst()) {
+							int nut = matrix[i][j].tree.pollFirst();
+							grow.push(nut + 1);
+							matrix[i][j].nurtient -= nut;
+							index = z + 1;
+						}else {
+							break;
+						}
+					}
+					for(int z = index; z < size; z++) {
+						matrix[i][j].dead.add(matrix[i][j].tree.pollLast());
+					}
+					while(!grow.isEmpty()) {
+						matrix[i][j].tree.offerFirst(grow.pop());
+					}
+				}
+
+			}
+		}
+
+		for(int i = 1; i <= N; i++) { //여름
+			for(int j = 1; j <= N; j++) {
+				if(!matrix[i][j].dead.isEmpty()) {
+					for(int nutr : matrix[i][j].dead) {
+						matrix[i][j].nurtient += nutr / 2;
+					}
+					matrix[i][j].dead.clear();
+				}
+			}
+
+		}
+
+		for(int i = 1; i <= N; i++) { //가을
+			for(int j = 1; j <= N; j++) {
+				for(int tree : matrix[i][j].tree) {
+					if(tree % 5 == 0) {
+						for(int z = 0; z < 8; z++) {
+							int nextX = i + dx[z];
+							int nextY = j + dy[z];
+							if(nextX > 0 && nextY > 0 && nextX <= N && nextY <= N) {
+								matrix[nextX][nextY].tree.offerFirst(1);
+							}
+						}
+					}
+				}
+			}
+
+		}
+
+		for(int i = 1; i <= N; i++) { //겨울
+			for(int j = 1; j <= N; j++) {
+				matrix[i][j].nurtient += matrix[i][j].increase;
+			}
+
+		}
 	}
 
 }
